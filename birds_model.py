1a3e50b6e0cce53dd1c702f87a7a3ef48403ded3
savanaconda <savannah.schilling@gmail.com>
2018-10-19 18:28:42 -0700

    Finished sightings over time plot

--- birds_model.py
+++ birds_model.py
@@ -8,6 +8,7 @@ import time
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.ticker import MultipleLocator
+from datetime import datetime, timedelta
 
 
 file = 'model_birddata.csv'
@@ -22,9 +23,8 @@ print('Read File:', time.time()-st)
 # test and train data
 ###################################
 
-print(data.head(1))
 
-
+# Plots coordinates on a globe
 def plot_3D_coords(coords_data, col_names):
 
 	# Include blue circle as background
@@ -44,6 +44,7 @@ def plot_3D_coords(coords_data, col_names):
 	plt.axis('off')
 
 
+# Plots coordinates on a world map
 def plot_gps_coords(gps_data, col_names):
 
 	# Include world map as background
@@ -60,7 +61,58 @@ def plot_gps_coords(gps_data, col_names):
 	ax.set_ylabel('Latitude')
 
 
+# Gets a date from a correctly formatted string
+def getdateformat(string):
+	date = datetime.strptime(string, '%Y-%m-%d')
+	return date
 
+
+# Plots the number of sightings over time, binning dates
+def plot_sightings_over_time(dates_data,dates_cols, bin_freq, num_ticks):
+
+	# Sort dates in order from oldest to newest
+	dates_data = dates_data.sort_values('SightingDate')
+
+	# Transform SightingDate column from strings to dates
+	dates_data['SightingDate'] = dates_data['SightingDate'].apply(getdateformat)
+
+	# Creates binned date entries
+	min_val = dates_data['SightingDate'].iloc[0]
+	max_val = dates_data['SightingDate'].iloc[-1]
+	bins_dt = pd.date_range(start=min_val,end=max_val, freq=bin_freq)
+	bins_str = bins_dt.astype(str).values
+	labels = ['({}, {}]'.format(bins_str[i-1], bins_str[i]) for i in range(1, len(bins_str))]
+	binned = pd.DataFrame()
+	binned['BinnedDates'] = pd.cut(dates_data['SightingDate'].astype(np.int64)//10**9,
+                   bins=bins_dt.astype(np.int64)//10**9,
+                   labels=labels)
+	binned = binned.reset_index(drop=True)
+
+	# Adds the count of sightings to each bin (getting histogram data)
+	dates_hist = binned.groupby('BinnedDates').size()
+
+	# Plot data
+	fig, ax = plt.subplots()
+	ax = dates_hist.plot.bar(color='y')
+
+	# Sets xticks and xlabels (specified by num_ticks)
+	interval = round(len(dates_hist)/num_ticks)
+	labels = dates_hist.iloc[::interval].index.values
+	labels_format = []
+	for val in labels:
+		split = val.split(',')
+		labels_format.append(split[0] + ' to \n' + split[1])
+	# if you delete this, then you don't love me - max
+	ticks = np.arange(0, len(dates_hist), interval)
+
+	# Format plot
+	ax.set_xticks(ticks)
+	ax.set_xticklabels(labels_format, rotation=90, ha='center')
+	plt.subplots_adjust(bottom=0.30)
+	ax.set_title('Number of Sightings Over Time')
+	ax.set_xlabel('Date ranges')
+	ax.set_ylabel('Number of Sightings')
+	
 
 
 def main():
@@ -79,13 +131,12 @@ def main():
 	dates_cols = ['SightingDate', 'AbsoluteDate']
 	dates_data = data[dates_cols].copy()
 
-	dates_hist = dates_data.groupby(dates_data['SightingDate']).count()
-	dates_hist = np.log10(dates_hist)
-	dates_hist.plot(kind='bar',color='y')
-	majorLocator = MultipleLocator(20)
-	plt.xaxis.set_major_locator(majorLocator)
-	plt.xticks(rotation=90)
-	plt.subplots_adjust(bottom=0.25)
+	# Sets the number of days per bin
+	bin_freq = '200D'
+	num_ticks = 12
+	plot_sightings_over_time(dates_data,dates_cols, bin_freq, num_ticks)
+
+
 
 
 	plt.show()
